c9cadf21a0b8820b3e3299412349e64bf0622c74
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-02-18 13:51:53 +0500

    final

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -x
 cd /opt/docker
 docker build -t valaxy_demo .
-docker tag thedockerguru03 thedockerguru03/valaxy_demo
+docker tag valaxy_demo thedockerguru03/valaxy_demo
 docker push thedockerguru03/valaxy_demo
 docker rmi valaxy_demo thedockerguru03/valaxy_demo
