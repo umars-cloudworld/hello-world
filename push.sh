fe865057529453f3fc18da42435dacd00529196e
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-02-18 13:34:09 +0500

    typos

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 set -x
 cd /opt/docker
 docker build -t valaxy_demo .
-docker tag thdockerguru03 thdockerguru03/valaxy_demo
-docker push thdockerguru03/valaxy_demo
-docker rmi valaxy_demo thdockerguru03/valaxy_demo
+docker tag thedockerguru03 thedockerguru03/valaxy_demo
+docker push thedockerguru03/valaxy_demo
+docker rmi valaxy_demo thedockerguru03/valaxy_demo
